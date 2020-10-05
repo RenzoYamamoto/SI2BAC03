6b432b0243d2c8389d8e84f39b93954df4dd7e7d
RenzoYamamoto <carlos.renzo@aluno.faculdadeimpacta.com.br>
2020-10-05 15:47:00 -0300

    ac03

--- primos.py
+++ primos.py
@@ -24,4 +24,30 @@ def nao_entre_em_panico():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=portimport os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(_name_)
+
+@app.route('/')
+def nao_entre_em_panico():
+    primos = "2"
+    num =  3
+    cont = 0
+    primo = True
+    while(cont < 99):
+        for i in range(2, num-1, 1 ):
+            if num % i == 0:
+                primo = False
+        if primo:
+            primos += "," + str(num)
+            cont += 1
+        num += 1
+        primo = True
+
+    return primos
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
