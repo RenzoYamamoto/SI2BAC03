aa069f886eda1771e6bc31d03fbe39730b536eb3
RenzoYamamoto <carlos.renzo@aluno.faculdadeimpacta.com.br>
2020-10-05 15:56:52 -0300

    ac03

--- primos.py
+++ primos.py
@@ -48,6 +48,6 @@ def nao_entre_em_panico():
 
     return primos
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
