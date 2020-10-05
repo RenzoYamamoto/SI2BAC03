4833ae9a0865305e1b2ae0f9f521429fe3c330c5
RenzoYamamoto <carlos.renzo@aluno.faculdadeimpacta.com.br>
2020-10-05 16:16:49 -0300

    ac03

--- primos.py
+++ primos.py
@@ -2,32 +2,6 @@ import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
-app = Flask(__name__)
-
-@app.route('/')
-def nao_entre_em_panico():
-    primos =  "2"
-    cont = 3
-    cont = 0
-    primo = True
-    while(cont < 99):
-        for i in range(2, num-1, 1 ):
-            if num % i == 0:
-                primo = False
-        if primo:
-            primos += "," + str(num)
-            cont += 1
-        num += 1
-        primo = True
-
-    return primos
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=portimport os
-from flask import Flask, jsonify, request
-from math import sqrt
-
 app = Flask(_name_)
 
 @app.route('/')
@@ -48,6 +22,6 @@ def nao_entre_em_panico():
 
     return primos
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
